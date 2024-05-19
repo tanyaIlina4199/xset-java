e01b7244a210be08748659f326b53a020909b7ea
tanyaIlina <4199ti@gmail.com>
2024-05-19 17:25:12 +0400

    added hw 21

--- com/xset/my/txthandler/TxtHandler.java
+++ com/xset/my/txthandler/TxtHandler.java
@@ -1,6 +1,7 @@
 package com.xset.my.txthandler;
 
 import java.io.*;
+import java.util.*;
 
 public class TxtHandler {
 
@@ -9,7 +10,70 @@ public class TxtHandler {
 
     public static void main(String[] args) {
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"));
+        try (BufferedReader reader = new BufferedReader(new FileReader("text.txt"))) {
+
+            Map<String, Integer> wordToCount = new HashMap<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] words = line.split("[', ]");
+                for (String word : words) {
+
+                    if (word.isBlank()) {
+                        continue;
+                    }
+
+                    word = checkShortForm(word);
+                    word = word.toLowerCase();
+
+                    wordToCount.put(word, wordToCount.getOrDefault(word, 0) + 1);
+                }
+            }
+
+            Map<String, Integer> wordToCountTree = new TreeMap<>(wordToCount);
+            Set<String> allWords = wordToCountTree.keySet();
+            System.out.println(allWords);
+            System.out.println(wordToCountTree);
+
+            int max = getMax(wordToCount);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("Words with max usage in text: ").append("\n");
+            for (Map.Entry<String, Integer> wordToCountEntry : wordToCount.entrySet()) {
+                Integer count = wordToCountEntry.getValue();
+                if (count.equals(max)) {
+                    sb.append(wordToCountEntry.getKey()).append(" uses ").append(count).append(" times;").append("\n");
+                }
+            }
+
+            System.out.println(sb);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int getMax(Map<String, Integer> wordToCount) {
+        int max = 0;
+        for (Integer value : wordToCount.values()) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    private static String checkShortForm(String word) {
+        switch (word) {
+            case "t" -> word = "not";
+            case "ll" -> word = "will";
+            case "s" -> word = "is";
+        }
+        return word;
+    }
+
+    private static void letterCounter() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("text1.txt"));
              BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
 
             String line;
